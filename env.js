56126844b8baacad7db56208f14c49d3a17b777a
Xiao258369 <129113465+Xiao258369@users.noreply.github.com>
2023-05-10 05:32:12 +0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,27 @@
+window.settings = {
+  // 站点标题
+  title: '出猴',
+  // 站点描述
+  description: '',
+  // API
+  host: 'http://lingshi666.xyz',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'default'
+  },
+  // 背景
+  background_url: 'http://lingshi666.xyz',
+  // crisp
+  crisp_id: '',
+  i18n: [
+    'zh-CN',
+    'en-US',
+    'ja-JP',
+    'vi-VN',
+    'ko-KR',
+    'zh-TW',
+    'fa-IR'
+  ]
+}
